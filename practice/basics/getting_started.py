9cc15aa7f070777d3d242dbbb2125a3b869a228e
“mohanraj” <mohanrj1345@gmail.com>
2022-03-23 14:10:48 +0530

    code reformatted using Black

--- practice/basics/getting_started.py
+++ practice/basics/getting_started.py
@@ -9,16 +9,19 @@ with sync_playwright() as pw:
     print(page.title())
     browser.close()
 
-    
+
 # # asyncio method
 import asyncio
 from playwright.async_api import async_playwright
+
+
 async def main():
     async with async_playwright() as apw:
-        browser = await apw.firefox.launch(headless= True)
+        browser = await apw.firefox.launch(headless=True)
         page = await browser.new_page()
         await page.goto("https://github.com/")
-        print (await page.title())
+        print(await page.title())
         await browser.close()
+
 
 asyncio.run(main())
--- practice/basics/take_screenshoot.py
+++ practice/basics/take_screenshoot.py
@@ -3,9 +3,8 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.firefox.launch(headless = True, slow_mo= 40)
+    browser = p.firefox.launch(headless=True, slow_mo=40)
     page = browser.new_page()
     page.goto("https://playwright.dev")
-    page.screenshot(path = "F:\\playwright_python\\results\\scrnshoot.jpeg")
+    page.screenshot(path="F:\\playwright_python\\results\\scrnshoot.jpeg")
     browser.close()
-    
